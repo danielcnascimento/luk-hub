534ff8ca5be1e774deb6c53d3d4d099795d69f97
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-03-03 15:26:56 -0300

    added List for api response

---
+++ src/components/List.js
@@ -0,0 +1,17 @@
+const List = () => {
+  return (
+    <div>
+      <div className="container">
+        <div>
+          <img className="imgContainer" src="" alt="" />
+        </div>
+        <div className="content">
+          <p>Daniel Nascimento</p>
+          <p>email@test.com</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default List;
