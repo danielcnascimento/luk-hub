94c032179b579eabb077375e4185891865d47211
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-03-09 16:39:26 -0300

    added Profile; added fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
+import Link from "next/link";
 import SearchBox from "../components/SearchBox";
 import styles from "../styles/pages/Home.module.css";
+import List from "../components/List";
 
 export default function Home() {
+  const id = 1;
+
   return (
     <div className={styles.container}>
       <Head>
@@ -12,7 +16,11 @@ export default function Home() {
         <div>
           <SearchBox />
         </div>
-        <div></div>
+        <div>
+          <Link href="/profile/id" as={`/profile/${id}`}>
+            <List />
+          </Link>
+        </div>
       </section>
     </div>
   );
