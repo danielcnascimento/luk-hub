f4cf8628fdd7c5891db453d9bdf55d1a32da9442
danielcnascimento <ddaniel.nascimento@gmail.com>
2021-03-10 18:27:51 -0300

    🔍 added search; added response.

--- src/components/List.js
+++ src/components/List.js
@@ -1,14 +1,14 @@
 import styles from "../styles/components/List.module.css";
 import Link from "next/link";
 
-const List = () => {
+const List = (props) => {
   const id = 1;
 
   return (
     <div>
       <div className={styles.listContainer}>
         <header>danielcnascimento</header>
-        <img src="https://github.com/danielcnascimento.png" alt="" />
+        {/* <img src="https://github.com/danielcnascimento.png" alt="" /> */}
         <main>
           <p>email@test.com.br</p>
           <p>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import Head from "next/head";
-import Link from "next/link";
 import List from "../components/List";
 import SearchBox from "../components/SearchBox";
 import styles from "../styles/pages/Home.module.css";
-
 import { searchRepo } from "../services/githubService";
 
 export default function Home() {
@@ -21,10 +19,6 @@ export default function Home() {
     }
   };
 
-
-  const id = 1;
-
-
   return (
     <div className={styles.container}>
       <Head>
@@ -34,15 +28,15 @@ export default function Home() {
         <div>
           <SearchBox loadingRepos={loadRepos} />
         </div>
-
-//         {loading ? "loading.." : <div>{JSON.stringify(repos, null, 2)}</div>}
-
-        <div className={styles.secondSide}>
-          <List />
-          <List />
-          <List />
-          <List />
-        </div>
+        {loading
+          ? "loading.."
+          : repos.map((repo) => {
+              return (
+                <div key={repo.id} className={styles.secondSide}>
+                  <List name={repo.name} />
+                </div>
+              );
+            })}
       </section>
     </div>
   );
